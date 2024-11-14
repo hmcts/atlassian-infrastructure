ad1fe7568c81e6262296d549397ead94cf3ab451
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-11-14 10:33:32 +0000

    changing the resource group name

--- components/general/resource-group.tf
+++ components/general/resource-group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "atlassian_rg" {
   location = var.location
-  name     = "atlassian-int-${var.env}-rg"
+  name     = "atlassian-${var.env}-rg"
   tags     = module.ctags.common_tags
 }
