fadb152c4c27bee087f72d7262e7fefb3e4e2cdf
Louise Huyton <louise.huyton@justice.gov.uk>
2024-11-20 17:49:21 +0000

    make changes to db tf

--- components/general/db.tf
+++ components/general/db.tf
@@ -10,10 +10,14 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = "RG-PRD-ATL-01"
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES-PASS-SOURCE" {
-  name         = "recipe-backend-POSTGRES-PASS-SOURCE"
-  value        = module.single_database_source.postgresql_password
+data "azurerm_key_vault_secret" "PRP-ATL-POSTGRES-PGSQL-BACKUP-TRIAL" {
+  name         = "PRP-ATL-POSTGRES-PGSQL-BACKUP-TRIAL"
   key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+output "secret_value" {
+  value     = data.azurerm_key_vault_secret.PRP-ATL-POSTGRES-PGSQL-BACKUP-TRIAL.value
+  sensitive = true
 }
 
 # single server (source) - for DMS migration testing only
@@ -23,8 +27,8 @@ module "single_database_source" {
   name               = "${var.product}-v11-source"
   location           = var.location
   env                = var.env
-  postgresql_user    = "rhubarbadmin"
-  database_name      = "rhubarb-v11"
+  postgresql_user    = "pgsqladmin"
+  database_name      = "backup-restore-trial"
   postgresql_version = "11"
   subnet_id          = "/subscriptions/b7d2bd5f-b744-4acc-9c73-e068cec2e8d8/resourceGroups/atlassian-nonprod-rg/providers/Microsoft.Network/virtualNetworks/atlassian-int-nonprod-vnet/subnets/atlassian-int-subnet-dat"
   sku_name           = "GP_Gen5_2"
@@ -32,7 +36,7 @@ module "single_database_source" {
   storage_mb         = "51200"
   common_tags        = module.ctags.common_tags
   subscription       = "b7d2bd5f-b744-4acc-9c73-e068cec2e8d8"
-  key_vault_rg       = "genesis-rg"
-  key_vault_name     = "dtssharedservices${var.env}kv"
+  key_vault_rg       = "RG-PRD-ATL-01"
+  key_vault_name     = "PRD-ATL-Backups-KV"
   business_area      = "SDS"
 }
