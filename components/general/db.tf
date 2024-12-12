0366860bd1ec7f3c4af4f2b1a0643cd929fcaf5b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-12-12 17:48:49 +0000

    trying to fix the Private end point

--- components/general/db.tf
+++ components/general/db.tf
@@ -48,7 +48,7 @@ resource "azurerm_postgresql_virtual_network_rule" "dat_subnet_rule" {
 }
 
 resource "azurerm_private_endpoint" "postgres_private_endpoint" {
-  name                = "atlassian-${var.env}-postgres-pe"
+  name                = "test-pe"
   location            = azurerm_resource_group.atlassian_rg.location
   resource_group_name = azurerm_resource_group.atlassian_rg.name
   subnet_id           = module.networking.subnet_ids["atlassian-int-${var.env}-vnet-atlassian-int-subnet-postgres"]
@@ -59,24 +59,6 @@ resource "azurerm_private_endpoint" "postgres_private_endpoint" {
     is_manual_connection           = false
     subresource_names              = ["postgresqlServer"]
   }
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "postgres_dns_zone_vnet_link" {
-  provider              = azurerm.dns
-  name                  = "atlassian-${var.env}-postgres-dns-vnet-link"
-  resource_group_name   = local.zone_resource_group
-  virtual_network_id    = module.networking.vnet_ids["atlassian-int-nonprod-vnet"]
-  private_dns_zone_name = local.zone_name
-}
-
-
-resource "azurerm_private_dns_a_record" "postgres_private_dns_a_record" {
-  provider            = azurerm.dns
-  name                = azurerm_postgresql_server.atlassian-server.name
-  zone_name           = local.zone_name
-  resource_group_name = local.zone_resource_group
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.postgres_private_endpoint.private_service_connection[0].private_ip_address]
 }
 
 
