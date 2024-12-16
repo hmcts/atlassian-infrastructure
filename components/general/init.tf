38111f8247f9fc7dd94c2b53060646d4a22bd621
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-16 09:56:52 +0000

    Add new provider config for vms

--- components/general/init.tf
+++ components/general/init.tf
@@ -9,9 +9,25 @@ terraform {
   backend "azurerm" {}
 }
 
-
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
+}
+
+provider "azurerm" {
+  alias = "soc"
+  features {}
+  subscription_id = "8ae5b3b6-0b12-4888-b894-4cec33c92292"
+}
+
+provider "azurerm" {
+  alias = "cnp"
+  features {}
+  subscription_id = var.cnp_vault_sub
+}
+
+provider "azurerm" {
+  alias = "dcr"
+  features {}
+  subscription_id = var.env == "prod" || var.env == "production" ? "8999dec3-0104-4a27-94ee-6588559729d1" : var.env == "sbox" || var.env == "sandbox" ? "bf308a5c-0624-4334-8ff8-8dca9fd43783" : "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
