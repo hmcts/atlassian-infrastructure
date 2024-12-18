685c9ff0a99f57d5d21602724d27a12ed6addd6a
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-18 11:59:24 +0000

    Add tags to disks

--- components/general/disk.tf
+++ components/general/disk.tf
@@ -7,4 +7,6 @@ resource "azurerm_managed_disk" "data_disk" {
   storage_account_type = each.value.storage_account_type
   storage_account_id   = "/subscriptions/b7d2bd5f-b744-4acc-9c73-e068cec2e8d8/resourceGroups/atlassian-nonprod-rg/providers/Microsoft.Storage/storageAccounts/atlassiannonprod"
   create_option        = each.value.create_option
+
+  tags = module.ctags.common_tags
 }
