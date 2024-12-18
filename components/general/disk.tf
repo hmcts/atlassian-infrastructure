a9ed825f6d776388e6dc05c8e787bbb15c3a9a50
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-18 13:31:11 +0000

    remove plan

--- components/general/disk.tf
+++ components/general/disk.tf
@@ -5,8 +5,13 @@ resource "azurerm_managed_disk" "data_disk" {
   resource_group_name  = azurerm_resource_group.atlassian_rg.name
   location             = "UK South"
   storage_account_type = each.value.storage_account_type
-  storage_account_id   = "/subscriptions/b7d2bd5f-b744-4acc-9c73-e068cec2e8d8/resourceGroups/atlassian-nonprod-rg/providers/Microsoft.Storage/storageAccounts/atlassiannonprod"
+  storage_account_id   = data.azurerm_storage_account.storage_account.id
   create_option        = each.value.create_option
 
   tags = module.ctags.common_tags
+}
+
+data "azurerm_storage_account" "storage_account" {
+  name                = "atlassian${var.env}"
+  resource_group_name = azurerm_resource_group.atlassian_rg.name
 }
