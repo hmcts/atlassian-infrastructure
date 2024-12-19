f7ac927daf198fd058cf028335f695e6a9767a06
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-19 09:19:50 +0000

    Add vnet link and import

--- components/general/import.tf
+++ components/general/import.tf
@@ -28,3 +28,9 @@ import {
   to = azurerm_network_interface.nic[each.key]
   id = "/subscriptions/b7d2bd5f-b744-4acc-9c73-e068cec2e8d8/resourceGroups/atlassian-nonprod-rg/providers/Microsoft.Network/networkInterfaces/${each.key}"
 }
+
+# VNET LINK IMPORTS
+import {
+  to = azurerm_private_dns_zone_virtual_network_link.postgres_vnet_link
+  id = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.postgres.database.azure.com/virtualNetworkLinks/atlassian-nonprod-postgres-dns-vnet-link"
+}
