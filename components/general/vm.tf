58339e09d8a173ba353ea4eeb766129489254042
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-12-20 08:25:37 +0000

    Add trigger_replaced value

--- components/general/vm.tf
+++ components/general/vm.tf
@@ -41,6 +41,10 @@ data "azurerm_key_vault_secret" "admin_username" {
 resource "terraform_data" "jira_vm" {
   for_each = { for k, v in var.vms : k => v if can(regex("jira", k)) }
 
+  triggers_replace = [
+    azurerm_virtual_machine.vm[each.key].id
+  ]
+
   connection {
     type        = "ssh"
     host        = azurerm_virtual_machine.vm[each.key].private_ip_address
