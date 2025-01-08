85b78585634cdd152bf91797c72fc60508b4539a
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-01-08 14:38:53 +0000

    Added peering

--- components/general/vnet-peering.tf
+++ components/general/vnet-peering.tf
@@ -18,3 +18,14 @@ resource "azurerm_virtual_network_peering" "dmz-to-int" {
   allow_virtual_network_access = "true"
   allow_forwarded_traffic      = "true"
 }
+
+
+resource "azurerm_virtual_network_peering" "int-to-ss-stg-vnet" {
+  name                      = "atlassian-int-nonprod-vnet-to-ss-stg-vnet"
+  resource_group_name       = azurerm_resource_group.atlassian_rg.name
+  virtual_network_name      = "atlassian-int-nonprod-vnet"
+  remote_virtual_network_id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/ss-stg-network-rg/providers/Microsoft.Network/virtualNetworks/ss-stg-vnet"
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
+}
