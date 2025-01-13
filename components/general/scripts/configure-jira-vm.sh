6c2c70265fefb9305e550b0f3c01b09d776559ba
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-01-13 10:10:20 +0000

    Further changes on the script

--- components/general/scripts/configure-jira-vm.sh
+++ components/general/scripts/configure-jira-vm.sh
@@ -19,8 +19,23 @@ chmod -R u+rw /opt/atlassian/jira
 # chmod -R u+rw /var/atlassian/application_data/jira_shared
 
 # # Update /etc/hosts
-if [ "$ENV" == "Staging" ]; then
+if [ "$ENV" == "nonprod" ]; then
   update_hosts_file_staging
+
+  # Replace glusterfs entry in /etc/fstab
+  sed -i '/glusterfs/c\10.0.4.150:/jira_shared /var/atlassian/application_data/jira_shared glusterfs defaults 0 0' /etc/fstab
+  mount -a
+
+  # Update Jira server.xml to replace tools.hmcts.net with staging.tools.hmcts.net
+  sed -i 's/proxyName="tools\.hmcts\.net"/proxyName="staging.tools.hmcts.net"/g' /opt/atlassian/jira/conf/server.xml
+
+  # Import SSL certificate
+
+    openssl s_client -connect staging.tools.hmcts.net:443 -servername staging.tools.hmcts.net < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /opt/atlassian/jira/jre/public.crt
+    /opt/atlassian/jira/jre/bin/keytool -importcert -alias staging.tools.hmcts.net -keystore /opt/atlassian/jira/jre/lib/security/cacerts -file /opt/atlassian/jira/jre/public.crt -storepass changeit
+
+    # Remove Dynatrace
+    /opt/dynatrace/oneagent/agent/uninstall.sh
 else
   echo "No environment specified"
 fi
@@ -33,15 +48,6 @@ nameserver 168.63.129.16
 "
 echo "${RESOLV_CONF_ENTRIES}" > /etc/resolv.conf
 
-# Replace glusterfs entry in /etc/fstab
-if [ "$ENV" == "Staging" ]; then
-  sed -i '/glusterfs/c\10.0.4.150:/jira_shared /var/atlassian/application_data/jira_shared glusterfs defaults 0 0' /etc/fstab
-else
-  echo "No environment specified"
-fi
-
-
-mount -a
 
 # Update dbconfig.xml
 for file in /var/atlassian/application_data/jira_shared/dbconfig.xml /opt/atlassian/jira/data/dbconfig.xml; do
@@ -50,29 +56,5 @@ for file in /var/atlassian/application_data/jira_shared/dbconfig.xml /opt/atlass
   sed -i "s|<password>.*</password>|<password>${DB_PASSWORD}</password>|" $file
 done
 
-# Update Jira server.xml to replace tools.hmcts.net with staging.tools.hmcts.net
-if [ "$ENV" == "Staging" ]; then
-  sed -i 's/proxyName="tools\.hmcts\.net"/proxyName="staging.tools.hmcts.net"/g' /opt/atlassian/jira/conf/server.xml
-else
-  echo "No changes needed"
-fi
-
-
-# Import SSL certificate
-if [ "$ENV" == "Staging" ]; then
-    openssl s_client -connect staging.tools.hmcts.net:443 -servername staging.tools.hmcts.net < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /opt/atlassian/jira/jre/public.crt
-    /opt/atlassian/jira/jre/bin/keytool -importcert -alias staging.tools.hmcts.net -keystore /opt/atlassian/jira/jre/lib/security/cacerts -file /opt/atlassian/jira/jre/public.crt -storepass changeit
-else
-  echo "No changes needed"
-fi
-
-
 
 systemctl start jira
-
-# Remove Dynatrace
-if [ "$ENV" == "Staging" ]; then
-  /opt/dynatrace/oneagent/agent/uninstall.sh
-else
-  echo "No changes needed"
-fi
