50633af97427f2be38fbef8337d6720dce710110
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2025-01-13 10:21:47 +0000

    Uninstalling Dynatrace from the jira VMs (#42)
    * Uninstalling Dyanatrace from the jira VMs
    
    * adding config to run for staging only
    
    * Further changes on the script

--- components/general/scripts/configure-jira-vm.sh
+++ components/general/scripts/configure-jira-vm.sh
@@ -10,6 +10,7 @@ source /tmp/functions.sh
 DB_URL=$1
 DB_USERNAME=$2
 DB_PASSWORD=$3
+ENV=$4
 
 # Grant permissions to Jira user
 chown -R jira:jira /opt/atlassian/jira
@@ -18,7 +19,27 @@ chmod -R u+rw /opt/atlassian/jira
 # chmod -R u+rw /var/atlassian/application_data/jira_shared
 
 # # Update /etc/hosts
-update_hosts_file
+if [ "$ENV" == "nonprod" ]; then
+  update_hosts_file_staging
+
+  # Replace glusterfs entry in /etc/fstab
+  sed -i '/glusterfs/c\10.0.4.150:/jira_shared /var/atlassian/application_data/jira_shared glusterfs defaults 0 0' /etc/fstab
+  mount -a
+
+  # Update Jira server.xml to replace tools.hmcts.net with staging.tools.hmcts.net
+  sed -i 's/proxyName="tools\.hmcts\.net"/proxyName="staging.tools.hmcts.net"/g' /opt/atlassian/jira/conf/server.xml
+
+  # Import SSL certificate
+
+    openssl s_client -connect staging.tools.hmcts.net:443 -servername staging.tools.hmcts.net < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /opt/atlassian/jira/jre/public.crt
+    /opt/atlassian/jira/jre/bin/keytool -importcert -alias staging.tools.hmcts.net -keystore /opt/atlassian/jira/jre/lib/security/cacerts -file /opt/atlassian/jira/jre/public.crt -storepass changeit
+
+    # Remove Dynatrace
+    /opt/dynatrace/oneagent/agent/uninstall.sh
+else
+  echo "No environment specified"
+fi
+
 
 # Update /etc/resolv.conf
 RESOLV_CONF_ENTRIES="
@@ -27,10 +48,6 @@ nameserver 168.63.129.16
 "
 echo "${RESOLV_CONF_ENTRIES}" > /etc/resolv.conf
 
-# Replace glusterfs entry in /etc/fstab
-sed -i '/glusterfs/c\10.0.4.150:/jira_shared /var/atlassian/application_data/jira_shared glusterfs defaults 0 0' /etc/fstab
-
-mount -a
 
 # Update dbconfig.xml
 for file in /var/atlassian/application_data/jira_shared/dbconfig.xml /opt/atlassian/jira/data/dbconfig.xml; do
@@ -39,12 +56,5 @@ for file in /var/atlassian/application_data/jira_shared/dbconfig.xml /opt/atlass
   sed -i "s|<password>.*</password>|<password>${DB_PASSWORD}</password>|" $file
 done
 
-# Update Jira server.xml to replace tools.hmcts.net with staging.tools.hmcts.net
-sed -i 's/proxyName="tools\.hmcts\.net"/proxyName="staging.tools.hmcts.net"/g' /opt/atlassian/jira/conf/server.xml
-
-# Import SSL certificate
-
-openssl s_client -connect staging.tools.hmcts.net:443 -servername staging.tools.hmcts.net < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /opt/atlassian/jira/jre/public.crt
-/opt/atlassian/jira/jre/bin/keytool -importcert -alias staging.tools.hmcts.net -keystore /opt/atlassian/jira/jre/lib/security/cacerts -file /opt/atlassian/jira/jre/public.crt -storepass changeit
 
 systemctl start jira
