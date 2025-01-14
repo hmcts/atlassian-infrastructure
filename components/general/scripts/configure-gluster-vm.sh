1fbe4f0435ec26a9d14ef588531f598e1c7e943b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-01-14 15:37:00 +0000

    Further script changes

--- components/general/scripts/configure-gluster-vm.sh
+++ components/general/scripts/configure-gluster-vm.sh
@@ -14,10 +14,8 @@ else
 fi
 
 # Update /etc/resolv.conf
-RESOLV_CONF_ENTRIES="
-search ygysg2ix1xfehcfemfnemkbkwe.zx.internal.cloudapp.net
-nameserver 168.63.129.16
-"
+RESOLV_CONF_ENTRIES="search ygysg2ix1xfehcfemfnemkbkwe.zx.internal.cloudapp.net
+nameserver 168.63.129.16"
 echo "${RESOLV_CONF_ENTRIES}" > /etc/resolv.conf
 
 log_entry "Updated resolv.conf"
