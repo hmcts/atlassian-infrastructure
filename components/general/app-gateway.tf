cde5f6a238198bedce9f1148db0143571614fa4a
Jordan Hoey <jordan.hoey@justice.gov.uk>
2025-01-31 13:53:16 +0000

    adding default rule to url_path_map

--- components/general/app-gateway.tf
+++ components/general/app-gateway.tf
@@ -125,6 +125,7 @@ resource "azurerm_application_gateway" "ag" {
       name                               = "appgw-url-map-path"
       default_backend_address_pool_name  = url_path_map.value.default_backend_address_pool_name
       default_backend_http_settings_name = url_path_map.value.default_backend_http_settings_name
+      default_rewrite_rule_set_name      = "Test-Rewrites"
       dynamic "path_rule" {
         for_each = [for p in url_path_map.value.path_rule : {
           name                       = p.name
