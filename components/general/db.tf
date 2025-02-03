d4d2d4932a41f3f936e693707c2de2f0d9d47fe4
Jordan Hoey <jordan.hoey@justice.gov.uk>
2025-02-03 15:09:44 +0000

    adding private endpoint config

--- components/general/db.tf
+++ components/general/db.tf
@@ -121,8 +121,8 @@ data "azurerm_key_vault_secret" "POSTGRES-FLEX-SERVER-USER" {
   key_vault_id = azurerm_key_vault.atlassian_kv.id
 }
 
-resource "azurerm_postgresql_flexible_server" "atlassian-flexible-server" {
-  name                = "atlassian-${var.env}-server"
+resource "azurerm_postgresql_flexible_server" "atlassian-flex-server" {
+  name                = "atlassian-${var.env}-flex-server"
   location            = azurerm_resource_group.atlassian_rg.location
   resource_group_name = azurerm_resource_group.atlassian_rg.name
   sku_name            = "MO_Standard_E8s_v3" # Memory Optimized SKU
@@ -140,5 +140,24 @@ resource "azurerm_postgresql_flexible_server" "atlassian-flexible-server" {
     ]
   }
 
+  tags = module.ctags.common_tags
+}
+
+resource "azurerm_private_endpoint" "postgres_flex_private_endpoint" {
+  name                = "atlassian-${var.env}-postgres-flex-pe"
+  location            = azurerm_resource_group.atlassian_rg.location
+  resource_group_name = azurerm_resource_group.atlassian_rg.name
+  subnet_id           = module.networking.subnet_ids["atlassian-int-${var.env}-vnet-atlassian-int-subnet-postgres"]
+
+  private_service_connection {
+    name                           = "postgres-flex-psc"
+    private_connection_resource_id = azurerm_postgresql_server.atlassian-flex-server.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
+  }
+  private_dns_zone_group {
+    name                 = local.zone_name
+    private_dns_zone_ids = [local.private_dns_zone_id]
+  }
   tags = module.ctags.common_tags
 }
