36dbe20f1b420af1fbf829312ef00aa45c7fbbb4
Jordan Hoey <jordan.hoey@justice.gov.uk>
2025-02-12 09:56:19 +0000

    removing unused provider refs

--- components/general/init.tf
+++ components/general/init.tf
@@ -19,21 +19,3 @@ provider "azurerm" {
   features {}
   subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
 }
-
-provider "azurerm" {
-  alias = "stg"
-  features {}
-  subscription_id = "74dacd4f-a248-45bb-a2f0-af700dc4cf68" #DTS-SHAREDSERVICES-STG
-}
-
-provider "azurerm" {
-  alias = "prod"
-  features {}
-  subscription_id = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c" #DTS-SHAREDSERVICES-PROD
-}
-
-provider "azurerm" {
-  alias = "prod"
-  features {}
-  subscription_id = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c" #DTS-SHAREDSERVICES-PROD
-}
