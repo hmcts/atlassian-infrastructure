de4bcba8883c038b794403683653bc5878ad1ccd
Jordan Hoey <jordan.hoey@justice.gov.uk>
2025-02-24 09:59:59 +0000

    setting disks to restore

--- components/general/import_disks.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_virtual_machine_data_disk_attachment.data_disk_attachment.atlassianprodjira01-datadisk-000-20250212-144738
-  id = "/subscriptions/79898897-729c-41a0-a5ca-53c764839d95/resourceGroups/atlassian-prod-rg/providers/Microsoft.Compute/disks/atlassianprodjira01-datadisk-000-20250212-144738"
-}
