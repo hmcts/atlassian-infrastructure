3225f1d86cae3bf2bb1e9f8464971e21bd509276
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-02-25 16:12:11 +0000

    adding providers again

--- components/general/init.tf
+++ components/general/init.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 1.9"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.9.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 4.9.0"
+      configuration_aliases = [azurerm.cnp, azurerm.soc, azurerm.dcr]
     }
   }
   backend "azurerm" {}
