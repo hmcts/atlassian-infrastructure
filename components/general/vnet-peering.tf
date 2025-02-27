5460c96545454535488c5be38ac92f3165afd82c
Jordan Hoey <jordan.hoey@justice.gov.uk>
2025-02-27 16:00:15 +0000

    testing peering

--- components/general/vnet-peering.tf
+++ components/general/vnet-peering.tf
@@ -19,8 +19,32 @@ resource "azurerm_virtual_network_peering" "dmz-to-int" {
   allow_forwarded_traffic      = "true"
 }
 
+resource "azurerm_virtual_network_peering" "int-to-ss-stg-vnet" {
+  count                     = var.env == "nonprod" ? 1 : 0
+  name                      = "atlassian-int-${var.env}-vnet-to-ss-${var.ss-env}-vnet"
+  resource_group_name       = azurerm_resource_group.atlassian_rg.name
+  virtual_network_name      = "atlassian-int-${var.env}-vnet"
+  remote_virtual_network_id = "/subscriptions/${var.ss-env-sub}/resourceGroups/ss-${var.ss-env}-network-rg/providers/Microsoft.Network/virtualNetworks/ss-${var.ss-env}-vnet"
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
+}
+
+resource "azurerm_virtual_network_peering" "ss-stg-vnet-to-int" {
+  count                     = var.env == "nonprod" ? 1 : 0
+  provider                  = azurerm.stg
+  name                      = "ss-${var.ss-env}-vnet-to-atlassian-int-${var.env}-vnet"
+  resource_group_name       = "ss-${var.ss-env}-network-rg"
+  virtual_network_name      = "ss-${var.ss-env}-vnet"
+  remote_virtual_network_id = module.networking.vnet_ids["atlassian-int-${var.env}-vnet"]
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
+}
+
 
 resource "azurerm_virtual_network_peering" "int-to-ss-env-vnet" {
+  count                     = var.env == "prod" ? 1 : 0
   name                      = "atlassian-int-${var.env}-vnet-to-ss-${var.ss-env}-vnet"
   resource_group_name       = azurerm_resource_group.atlassian_rg.name
   virtual_network_name      = "atlassian-int-${var.env}-vnet"
@@ -31,6 +55,7 @@ resource "azurerm_virtual_network_peering" "int-to-ss-env-vnet" {
 }
 
 resource "azurerm_virtual_network_peering" "ss-env-vnet-to-int" {
+  count                     = var.env == "prod" ? 1 : 0
   provider                  = azurerm.stg
   name                      = "ss-${var.ss-env}-vnet-to-atlassian-int-${var.env}-vnet"
   resource_group_name       = "ss-${var.ss-env}-network-rg"
