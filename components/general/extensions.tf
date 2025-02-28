6b0d4a3273e7af5073371cc526e8e14afaf51197
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-02-28 15:34:36 +0000

    adding count index to test resource

--- components/general/extensions.tf
+++ components/general/extensions.tf
@@ -11,7 +11,7 @@ module "vm-bootstrap" {
   source = "git::https://github.com/hmcts/terraform-module-vm-bootstrap.git?ref=DTSPO-24291-updating-dynatrace-settings"
 
   virtual_machine_type        = "vm"
-  virtual_machine_id          = azurerm_virtual_machine.vm_test.id
+  virtual_machine_id          = azurerm_virtual_machine.vm_test[count.index].id
   install_dynatrace_oneagent  = var.install_dynatrace_oneagent
   install_azure_monitor       = var.install_azure_monitor
   install_nessus_agent        = var.install_nessus_agent
@@ -20,7 +20,7 @@ module "vm-bootstrap" {
   run_command                 = var.run_command
   os_type                     = var.os_type
   env                         = var.env == "prod" ? var.env : "nonprod"
-  dynatrace_custom_hostname   = azurerm_virtual_machine.vm_test.name
+  dynatrace_custom_hostname   = azurerm_virtual_machine.vm_test[count.index].name
 
   common_tags = module.ctags.common_tags
 }
