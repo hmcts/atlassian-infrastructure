80829277c625ec1314cc9ac8320c51b726024e49
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-02-28 12:49:04 +0000

    updating cnp provider

--- components/general/init.tf
+++ components/general/init.tf
@@ -41,7 +41,7 @@ provider "azurerm" {
 provider "azurerm" {
   alias = "cnp"
   features {}
-  subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
+  subscription_id = var.env == "prod" ? "8999dec3-0104-4a27-94ee-6588559729d1" : "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
 
 provider "azurerm" {
