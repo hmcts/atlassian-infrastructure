3fa91d04dbcae7e480d38e6aef75be092bc93a69
Dawid Strozak <dawid.strozak@hmcts.net>
2025-04-14 16:01:47 +0100

    Set the correct api_key reference for the subuser provider

--- components/general/init.tf
+++ components/general/init.tf
@@ -24,7 +24,7 @@ provider "sendgrid" {
 
 provider "sendgrid" {
   alias   = "subuser"
-  api_key = data.azurerm_key_vault_secret.api.value
+  api_key = data.azurerm_key_vault_secret.sendgrid-terraform-api-key.value
   subuser = sendgrid_subuser.user.username
 }
 
