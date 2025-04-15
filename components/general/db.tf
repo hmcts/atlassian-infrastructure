d6a79e6c262d6be897fad70635f7f2a1f2dcf12a
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-04-15 16:21:39 +0100

    Add new terraform_data resource for nonprod v15

--- components/general/db.tf
+++ components/general/db.tf
@@ -235,3 +235,24 @@ resource "azurerm_postgresql_flexible_server" "atlassian-nonprod-flex-server-v15
 
   tags = module.ctags.common_tags
 }
+
+resource "terraform_data" "atlassian-nonprod-flex-server-v15" {
+  for_each = local.app_names
+
+  triggers_replace = [
+    azurerm_postgresql_flexible_server.atlassian-nonprod-flex-server-v15.id,
+    azurerm_key_vault_secret.postgres_password[each.key].id,
+    azurerm_key_vault_secret.postgres_username[each.key].id
+  ]
+  provisioner "local-exec" {
+    command = "./scripts/configure-postgres.sh"
+    environment = {
+      POSTGRES_HOST  = azurerm_postgresql_flexible_server.atlassian-nonprod-flex-server-v15.fqdn
+      ADMIN_USER     = data.azurerm_key_vault_secret.POSTGRES-FLEX-SERVER-USER.value
+      ADMIN_PASSWORD = data.azurerm_key_vault_secret.POSTGRES-FLEX-SERVER-PASS.value
+      DATABASE_NAME  = "${each.key}-db-${var.env}"
+      USER           = "${each.key}_user"
+      PASSWORD       = random_password.postgres_password[each.key].result
+    }
+  }
+}
