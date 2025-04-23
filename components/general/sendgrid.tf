b49b85d0a6f0691c7ef2d2499c47b2e5357c846d
Dawid Strozak <dawid.strozak@hmcts.net>
2025-04-23 11:16:31 +0100

    Fix import reference

--- components/general/sendgrid.tf
+++ components/general/sendgrid.tf
@@ -1,5 +1,5 @@
 import {
-  to = azurerm_template_deployment.sendgrid
+  to = azurerm_resource_group_template_deployment.sendgrid
   id = "/subscriptions/79898897-729c-41a0-a5ca-53c764839d95/resourceGroups/atlassian-prod-rg/providers/Microsoft.SaaS/resources/SGAAPPATL01"
 }
 
