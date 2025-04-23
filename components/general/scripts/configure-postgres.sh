c3d2e127e1b55beaa0c42495956890edaacf6c0b
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-04-23 18:01:15 +0100

    Update script to check if table exists before running query

--- components/general/scripts/configure-postgres.sh
+++ components/general/scripts/configure-postgres.sh
@@ -29,7 +29,15 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO \"${USER}\";
 psql "sslmode=require" -c "${SQL_COMMAND}"
 
 # Disable emails if DATABASE_NAME is jira-db-nonprod
-if [ "${DATABASE_NAME}" = "jira-db-nonprod" ]; then
-  DISABLE_EMAIL="UPDATE propertynumber SET propertyvalue = 1 WHERE \"id\" = (SELECT \"id\" FROM \"propertyentry\" WHERE \"property_key\" = 'jira.mail.send.disabled');"
-  psql "sslmode=require" -c "${DISABLE_EMAIL}"
+if [ "${DATABASE_NAME}" == "jira-db-nonprod" ]; then
+  # Check if the table 'propertynumber' exists
+  TABLE_EXISTS=$(psql "sslmode=require" -tAc "SELECT EXISTS (SELECT 1 FROM pg_catalog.pg_tables WHERE schemaname = 'public' AND tablename = 'propertynumber');")
+
+  # If the table exists, run the query to disable emails
+  if [ "${TABLE_EXISTS}" == "t" ]; then
+    DISABLE_EMAIL="UPDATE propertynumber SET propertyvalue = 1 WHERE \"id\" = (SELECT \"id\" FROM \"propertyentry\" WHERE \"property_key\" = 'jira.mail.send.disabled');"
+    psql "sslmode=require" -c "${DISABLE_EMAIL}"
+  else
+    echo "Table 'propertynumber' does not exist. Skipping email disable query."
+  fi
 fi
