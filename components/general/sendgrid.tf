c1c93c9635e554f3c18b4442805e7399e43e9b76
Dawid Strozak <dawid.strozak@hmcts.net>
2025-04-23 11:38:47 +0100

    Attempt to fix the id again

--- components/general/sendgrid.tf
+++ components/general/sendgrid.tf
@@ -1,6 +1,6 @@
 import {
   to = azurerm_resource_group_template_deployment.sendgrid
-  id = "/subscriptions/79898897-729c-41a0-a5ca-53c764839d95/resourceGroups/atlassian-prod-rg/providers/Microsoft.Resources/deployments/SGAAPPATL01"
+  id = "/subscriptions/79898897-729c-41a0-a5ca-53c764839d95/resourceGroups/atlassian-prod-rg/providers/Microsoft.SaaS/deployments/SGAAPPATL01"
 }
 
 resource "azurerm_resource_group_template_deployment" "sendgrid" {
