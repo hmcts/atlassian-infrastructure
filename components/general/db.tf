618e81413ce101c8deb4358fffbd53f3938fb1d3
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-04-24 15:08:08 +0100

    Remove dependsOn

--- components/general/db.tf
+++ components/general/db.tf
@@ -263,8 +263,4 @@ resource "terraform_data" "atlassian-flex-server-v15" {
       PASSWORD       = random_password.postgres_password[each.key].result
     }
   }
-
-  depends_on = [
-    azurerm_postgresql_flexible_server_database.v15-database
-  ]
 }
